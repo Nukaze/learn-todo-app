fd30453bb574c0934633085c2ad36154bbf369d6
Anupun Phob Khumthong <nukaze4@gmail.com>
2022-12-27 20:40:29 +0700

    refactor(code): state driven & colors
    - state driven change from _LoginPageState createState() => _LoginPageState(); -> State<LoginPage> createState() => _LoginPageState();
    - colors change some component and name attribute
    - remove color debugger

--- lib/login.dart
+++ lib/login.dart
@@ -1,33 +1,30 @@
+import 'dart:math';
+
 import 'package:flutter/material.dart';
 import 'main.dart';
 
 class LoginPage extends StatefulWidget {
+  const LoginPage({super.key});
   @override
-  _LoginPageState createState() => _LoginPageState();
+  State<LoginPage> createState() => _LoginPageState();
 }
 class _LoginPageState extends State<LoginPage>{
-  Color textColor = Palette.textRGBO;
-  Color text2 = Colors.amber;
-  String ts1 = Palette.textRGBO.toString();
-  String ts2 = Colors.amber.toString();
-  // Color test1 = MaterialColor(Palette.textRGBO);
   @override
   Widget build(BuildContext context){
-    print("pallette $ts1 colors $ts2");
-    // print("\n test1 $test1");
+
     return Container(
       padding: const EdgeInsets.only(top: 150,bottom: 100, left: 20, right: 20),
-      margin: const EdgeInsets.only(top: 0),
+      margin: const EdgeInsets.only(top: 40),
       alignment: Alignment.topCenter,
       constraints: const BoxConstraints(maxWidth: 160),
       decoration: BoxDecoration(
-        color: Palette.secondaryRGBO,
+        color: Palette.secondary,
         borderRadius: BorderRadius.circular(4),
         boxShadow: [
-          BoxShadow(color: Palette.contrastRGBO, blurRadius: 5),
+          BoxShadow(color: Palette.contrast, blurRadius: 10),
         ],
       ),
-      child: const Text("Login!",style: TextStyle(color: Colors.amber, fontSize: 56)),
+      child: const Text("Login!",style: TextStyle(color: Palette.text, fontSize: 56)),
     );
   }
 }
