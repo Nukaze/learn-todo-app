ab48c13079df199e0852002c320e7efe5d460413
Anupun Phob Khumthong <nukaze4@gmail.com>
2022-12-30 00:34:52 +0700

    init(file): create for helper to storing data
    - import sqflie(sqlite) & firebase
    - prepare both class to easy to using

---
+++ lib/database.dart
@@ -0,0 +1,13 @@
+import 'package:flutter/material.dart';
+import 'package:path/path.dart';
+import 'package:sqflite/sqflite.dart';
+import 'package:firebase_core/firebase_core.dart';
+import 'package:firebase_auth/firebase_auth.dart';
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'tools.dart';
+
+class getSqlite {
+  static Database? _database;
+}
+
+class getFirebase {}
