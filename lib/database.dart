243cb93d6dc52414a56d7055ff56b5da89fc8964
Anupun Phob Khumthong <nukaze4@gmail.com>
2023-01-01 21:24:46 +0700

    feat(create): create database file complete

--- lib/database.dart
+++ lib/database.dart
@@ -6,8 +6,37 @@ import 'package:firebase_auth/firebase_auth.dart';
 import 'package:cloud_firestore/cloud_firestore.dart';
 import 'tools.dart';
 
-class getSqlite {
+class Sqlitebase {
   static Database? _database;
+  static String? _databasePath;
+
+  _Sqlitebase() {}
+
+  Future<void> createDatabasePath(String? dbName) async {
+    if (dbName!.isEmpty) {
+      dbName = "my_database.db";
+    }
+    if (!dbName.contains(".db")) {
+      dbName += ".db";
+    }
+    _databasePath = await getDatabasesPath() + "/" + dbName;
+    dprint("db path = $_databasePath");
+  }
+
+  Future<void> createTable(String tableName) async {
+    try {
+      Database db = await openDatabase(
+        _databasePath!,
+        version: 1,
+        onCreate: (db, version) async {
+          await db.execute("CREATE TABLE $tableName(uid INTEGER PRIMARY KEY, email TEXT, password INTEGER);");
+        },
+      );
+      dprint("table has been created.");
+    } catch (e) {
+      dprint("Catch : $e");
+    }
+  }
 }
 
-class getFirebase {}
+class Firebase {}
