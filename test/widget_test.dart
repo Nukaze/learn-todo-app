f0a482c6f9c87a18bb6c309d50fee0b56564d636
Anupun Phob Khumthong <nukaze4@gmail.com>
2023-01-07 01:24:56 +0700

    build(dependencies): major build upgrade
    - change project name from FlutterIntroApp -> TodoApp
    - import path_provider
    - change app icon
    - chore build

--- test/widget_test.dart
+++ test/widget_test.dart
@@ -8,7 +8,7 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
 
-import 'package:FlutterIntroApp/main.dart';
+import 'package:TodoApp/main.dart';
 
 void main() {
   testWidgets('Counter increments smoke test', (WidgetTester tester) async {
