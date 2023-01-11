81b09546285150cd866caa66e8193321a9f222e2
Anupun Phob Khumthong <nukaze4@gmail.com>
2023-01-12 00:31:54 +0700

    feat(auth): password generator & checker
    - getAuthPasswordGenerate for make hashed password to collect in database
    - getAuthPasswordChecl for check both password are same for example user login email, password in process we use this to check password is valid from database where associate with user login email by generate by salt return by boolean datatype

---
+++ lib/authentication.dart
@@ -0,0 +1,13 @@
+import 'tools.dart';
+import 'package:bcrypt/bcrypt.dart';
+
+Map<String, String> getAuthPasswordGenerate(String password) {
+  final salt = BCrypt.gensalt();
+  final hashedPassword = BCrypt.hashpw(password, salt);
+  return {"password": hashedPassword, "salt": salt};
+}
+
+bool getAuthPasswordCheck(String checkPassword, String validPassword, String salt) {
+  final hashedCheckPassword = BCrypt.hashpw(checkPassword, salt);
+  return hashedCheckPassword == validPassword ? true : false;
+}
