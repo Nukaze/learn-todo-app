cfc72c208f962a4ead8f74dfa38360cf2b39fed6
Anupun Phob Khumthong <nukaze4@gmail.com>
2023-01-15 13:11:18 +0700

    init(file): create splash_screen for super init
    - create for banner or logo show loading when initial app

---
+++ lib/splash_screen.dart
@@ -0,0 +1,34 @@
+import 'package:flutter/material.dart';
+import 'tools.dart';
+
+class SplashScreen extends StatefulWidget {
+  const SplashScreen({super.key})
+  @override
+  State<SplashScreen> createState() => _SplashScreenState();
+}
+
+class _SplashScreenState extends State<SplashScreen> {
+  @override
+  void initState() {
+    super.initState();
+    Future.delayed(const Duration(seconds: 2), () {
+      Navigator.pushReplacementNamed(context, '/Login');
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: <Widget>[
+            // your logo or branding banner here
+            Image.asset('assets/logo/shiawase_goddess.png'),
+            const Text('Loading...'),
+          ],
+        ),
+      ),
+    );
+  }
+}
